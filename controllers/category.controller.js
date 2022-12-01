3e2b0f2bb35577eec9532fd2e9dd1bf726ff0a67
Chaitanya Kumar Adhikamsetty <chaitanya.adhikamsetty@preqin.com>
2022-12-01 19:12:10 +0530

    Updated the categories API.

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -3,11 +3,17 @@ const db = require("../dbOperations");
 
 // Retrieve all Categories from the database.
 exports.get = (req, res) => {
-  db.executeQuery(
-    "  select * from dev.category_1 ct1 join dev.category_2 ct2  on ct2.category_1_id = ct1.category_1_id join dev.category_3 ct3 on ct3.category_2_id = ct2.category_2_id"
-  )
+  let query = "select * from dev.category_1";
+  if (req.params.categoryLevel === "2") {
+    query =
+      "select ct1.category_1_id,ct1.category_1_name,ct1.category_1_img_uri, ct2.category_2_id,ct2.category_2_name,ct2.category_2_img_uri from dev.category_1 ct1 join dev.category_2 ct2  on ct2.category_1_id = ct1.category_1_id";
+  }
+  if (req.params.categoryLevel === "3") {
+    query =
+      "select ct1.category_1_id,ct1.category_1_name,ct1.category_1_img_uri, ct2.category_2_id,ct2.category_2_name,ct2.category_2_img_uri,ct3.category_3_id,ct3.category_3_name,ct3.category_3_img_uri  from dev.category_1 ct1 join dev.category_2 ct2  on ct2.category_1_id = ct1.category_1_id join dev.category_3 ct3 on ct3.category_2_id = ct2.category_2_id";
+  }
+  db.executeQuery(query)
     .then((data) => {
-      console.log(data);
       res.json(data[0]);
     })
     .catch((err) => {
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -4,7 +4,7 @@ const controller = require("../controllers/category.controller");
 const router = Router();
 
 // Retrieve all Categories
-router.get("/", controller.get);
+router.get("/:categoryLevel", controller.get);
 // // Retrieve a single Category with id
 // router.get("/:id", controller.getById);
 // // Delete a Category with id
