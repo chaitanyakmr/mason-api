cba9ed69228422a2a31ae2a02492930abfe9fab2
chaitanyakmr8 <chaitanyakmr8@gmail.com>
2022-12-19 23:06:59 +0530

    Updated the categories API from sql connection to postgres

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -22,9 +22,9 @@ exports.get = (req, res) => {
     query =
       "select ct1.category_1_id,ct1.category_1_name,ct1.category_1_img_uri, ct2.category_2_id,ct2.category_2_name,ct2.category_2_img_uri,ct3.category_3_id,ct3.category_3_name,ct3.category_3_img_uri  from dev.category_1 ct1 join dev.category_2 ct2  on ct2.category_1_id = ct1.category_1_id join dev.category_3 ct3 on ct3.category_2_id = ct2.category_2_id";
   }
-  db.get(query)
-    .then((data) => {
-      res.json(data[0]);
+  db.query(query)
+    .then((data) => { 
+      res.status(200).json(data.rows);
     })
     .catch((err) => {
       res.status(500).send({
--- dbOperations.js
+++ dbOperations.js
@@ -1,25 +1,44 @@
-var config = require("./config/sqldb.config");
-const sql = require("mssql");
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'pgadmin@mason-pgsql',
+  host: 'mason-pgsql.postgres.database.azure.com',
+  database: 'postgres',
+  password: 'Welcome@1',
+  port: 5432,
+  ssl: true,
+  dialectOptions: {
+     ssl: {
+        require: true
+     }
+   }
+});
 
 async function get(query) {
-    let pool = await sql.connect(config);
-    let res = await pool.request().query(query);
-    pool.close();
-    return res.recordsets; 
+ await pool.query(query, (error, results) => {
+    if (error) {
+      throw error
+    }
+   return  results.rows;
+  });
 }
 
 async function post(query, params) { 
-    let pool = await sql.connect(config);
-    let request = pool.request();
-    params.forEach(param => {
-      request.input(param.name, sql[param.dataType], param.value)
-    });
-    const res =await request.query(query);
-    pool.close();
-    return res.recordsets;
-  
+  // let pool = await sql.connect(config);
+  // let request = pool.request();
+  // params.forEach(param => {
+  //   request.input(param.name, sql[param.dataType], param.value)
+  // });
+  // const res =await request.query(query);
+  // pool.close();
+  //  res.recordsets;
+
+  let res = await pool.query(query, params.map(x => x.value), (error, results) => {
+    if (error) {
+      throw error
+    };
+    return results.rows;
+  });
+  return res;
 }
 
-module.exports = {
-  get,post
-};
+module.exports = pool;
